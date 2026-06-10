d7aa7a45c60f20725c3868f92bb733a7e68a44c5
harshita-1410 <harshita.sisodiya133635@marwadiuniversity.ac.in>
2026-06-10 12:46:56 +0530

    Add files via upload

---
+++ LAB1.php
@@ -0,0 +1,18 @@
+<?php
+
+define("COLLEGE_NAME", "ABC College");
+
+$studentName = "Rahul Patel";
+$semester = "Semester 3";
+$marks = 450;
+$totalMarks = 500;
+$percentage = ($marks / $totalMarks) * 100;
+
+echo "<h2>Previous Semester Result</h2>";
+echo "College Name: " . COLLEGE_NAME . "<br>";
+echo "Student Name: " . $studentName . "<br>";
+echo "Semester: " . $semester . "<br>";
+echo "Marks Obtained: " . $marks . " / " . $totalMarks . "<br>";
+echo "Percentage: " . $percentage . "%<br>";
+?>
+
